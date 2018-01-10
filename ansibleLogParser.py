5cb81c6a3b72d663715c570af8788c04ace29ca7
Branden Pleines <bpleines@redhat.com>
2018-01-10 11:03:04 -0800

    spelling here too

--- ansibleLogParser.py
+++ ansibleLogParser.py
@@ -7,7 +7,7 @@ def printSummary():
     hitRecap = False
     successes = []
     failures = []
-    unreacheables = []
+    unreachables = []
     with open(logfile, 'r') as log:
         for line in log:
             if 'PLAY RECAP' in line and not hitRecap:
@@ -15,8 +15,8 @@ def printSummary():
             elif hitRecap:
                 nextLine = line[6:]
                 hostname, tasks = nextLine.split('[0m', 1)
-                if 'unreacheable=1' in tasks:
-                    unreacheables.append(hostname)
+                if 'unreachable=1' in tasks:
+                    unreachables.append(hostname)
                 elif re.search('failed=[1-9+]', tasks):
                     failures.append(hostname)
                 else:
@@ -30,8 +30,8 @@ def printSummary():
     print(f"\nFailures: {len(failures)}")
     for failure in failures:
         print(failure)
-    print(f"\nUnreacheables: {len(unreacheables)}")
-    for unreacheable in unreacheables:
-        print(unreacheable)
+    print(f"\nUnreacheables: {len(unreachables)}")
+    for unreachable in unreachables:
+        print(unreachable)
 
 printSummary()
